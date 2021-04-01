ccdf53eefd5b10041b8dc12ea00fa2edee726cd5
Svetlana Arustamyan <lampa1979@mail.ru>
2021-04-01 15:54:36 +0400

    Изменен main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let income = 100;
 let addExpenses = -10;
 let deposit = 20;
 let mission = 5;
-const period = 12;
+const PERIOD = 12;
 alert('Hello, world!');
 console.log(' Сумма денежных средств и полученного дохода составляет:' + (money + income));
 console.log(' Сумма денежных средств, полученного дохода и депозита составляет:');
