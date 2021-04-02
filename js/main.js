370834341272cc26c0abf71abee836a2e14f2595
Svetlana Arustamyan <lampa1979@mail.ru>
2021-04-02 11:47:45 +0400

    Change main.js for lesson02

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+// доход за месяц
 let money = 800;
+// доп.доход
 let income = 'Фриланс';
-let addExpenses = 'Коммунальные платежи, расходы на транспорт, мобильный телефон, интернет';
+// доп. расход
+let addExpenses = 'Коммунальные Платежи, Расходы на Транспорт, Мобильный Телефон, Интернет';
 let deposit = true;
+// цель накопить
 let mission = 500;
+//  период попытки
 let period = 12;
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' фунтов стерлингов');
+console.log((addExpenses.toLowerCase()), (addExpenses.split(', ')));
+
+let budgetDay;
+budgetDay = money / 30;
+console.log(budgetDay);
