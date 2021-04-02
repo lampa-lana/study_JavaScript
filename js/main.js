cfcfc3837f59a5bfd9ad120fcc85344d385041ca
Svetlana Arustamyan <lampa1979@mail.ru>
2021-04-02 13:22:10 +0400

    Create lesson02-addTask, index_add.html, main__add.js & styles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // доход за месяц
 let money = 800;
 // доп.доход
-let income = 'Фриланс';
+let income = "Фриланс";
 // доп. расход
-let addExpenses = 'Коммунальные Платежи, Расходы на Транспорт, Мобильный Телефон, Интернет';
+let addExpenses =
+  "Коммунальные Платежи, Расходы на Транспорт, Мобильный Телефон, Интернет";
 let deposit = true;
 // цель накопить
 let mission = 500;
@@ -14,9 +15,9 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' фунтов стерлингов');
-console.log((addExpenses.toLowerCase()), (addExpenses.split(', ')));
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " фунтов стерлингов");
+console.log(addExpenses.toLowerCase(), addExpenses.split(", "));
 
 let budgetDay;
 budgetDay = money / 30;
