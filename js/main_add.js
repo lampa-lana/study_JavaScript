ea55d7995775886b00b91084fb59e7eeddd6242d
Svetlana Arustamyan <lampa1979@mail.ru>
2021-04-05 15:23:33 +0400

    change main_add.js

--- js/main_add.js
+++ js/main_add.js
@@ -1,3 +1,5 @@
+'use strict'
+
 let lang = 'ru';
 if (lang === 'ru') {
   console.log('понедельник', 'вторник', 'среда', 'чтверг', 'пятница', 'суббота', 'воскресенье');
@@ -5,11 +7,11 @@ if (lang === 'ru') {
   console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 } else{
   console.log('Выбрать корректное значение языка');
-};
+}
 
 switch (lang) {
   case 'ru':
-    console.log('понедельник', 'вторник', 'среда', 'чтверг', 'пятница', 'суббота', 'воскресенье');
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
     break;
  case 'en':
     console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
@@ -17,8 +19,18 @@ switch (lang) {
   default:
     console.log('Выбрать корректное значение языка');
     break;
-};
+}
 
-let namePerson = 'Светлана';
+let arr = [
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+];
+lang = true ? arr[0] : arr[1];
+console.log(lang);
+
+
+let namePerson = 'Максим';
 let result = namePerson === 'Артем' ? 'Директор' : 'Студент' && namePerson === 'Максим' ? 'Преподаватель' : 'Студент';
 console.log(result);
+
+console.log(namePerson === 'Артем' ? 'Директор' : 'Студент' && namePerson === 'Максим' ? 'Преподаватель' : 'Студент');
